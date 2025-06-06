eadbff0602ee8424fb25fde9e0704d0287e910ac
asad-ullah-turab <asadullahturab47@gmail.com>
2025-06-06 21:51:42 +0500

    Fix: mason lspconfig error and remove smear-cursor and vimtex

--- lua/MrLmao/plugins/vimtex.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lervag/vimtex",
-  ft = "tex", -- Load only for TeX files
-  config = function()
-    vim.g.vimtex_view_method = "zathura" -- Use Zathura as PDF viewer (change if needed)
-    vim.g.vimtex_compiler_method = "latexmk" -- Use latexmk for compilation
-    vim.g.tex_flavor = "latex"
-  end,
-}
